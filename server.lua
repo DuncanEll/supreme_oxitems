a2fda6a45421dbbcd325fafc1c5039c53843bc21
DuncanEll <127456850+DuncanEll@users.noreply.github.com>
2023-05-02 17:09:01 +0100

    added ace permissions

--- server.lua
+++ server.lua
@@ -1,5 +1,7 @@
+
 RegisterServerEvent('supreme_oxitem:addItem')
 AddEventHandler('supreme_oxitem:addItem', function(name, label2, weight, stack, close, description)
+	if IsPlayerAceAllowed(source, "oxitems") then
 	local src = source
 	local path = GetResourcePath(GetCurrentResourceName())
 	path = path:gsub('//', '/')..'/createditems.lua'
@@ -16,4 +18,14 @@ AddEventHandler('supreme_oxitem:addItem', function(name, label2, weight, stack, 
 )
 	file:write('},')
 	file:close()
+end
+end)
+
+
+RegisterCommand('oxadditem', function(source)
+    if IsPlayerAceAllowed(source, "oxitems") then
+        TriggerClientEvent("supreme_oxitems:openMenu", source)
+	else
+		print('not allowed')
+    end
 end)
