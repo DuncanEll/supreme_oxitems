86ab92777c1209ff4f4c47b6dfc29583f174426d
DuncanEll <127456850+DuncanEll@users.noreply.github.com>
2023-05-04 12:47:30 +0100

    Update client.lua
    added image url

--- client.lua
+++ client.lua
@@ -44,11 +44,18 @@ AddEventHandler('supreme_oxitems:openMenu', function()
             label = "Description",
             icon = "",
             required = true
+         },
+         {
+            type = "input",
+            label = "Image URL",
+            icon = "",
+            required = true
          }
     })
     if data == nil then
       print('it was nil')
     else
  TriggerServerEvent('supreme_oxitem:addItem', data[1], data[2], data[3], data[4], data[5], data[6])
+ TriggerServerEvent('supreme_oxitem:addImage', data[1], data[7])
     end
 end)
