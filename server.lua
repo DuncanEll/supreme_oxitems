f8853671ca15d28c5160663676b56157d3ca950b
DuncanEll <127456850+DuncanEll@users.noreply.github.com>
2023-05-04 13:47:44 +0100

    Image URL Feature Added & Direct save item to ox_inventory items.lua

--- server.lua
+++ server.lua
@@ -2,20 +2,35 @@ RegisterServerEvent('supreme_oxitem:addItem')
 AddEventHandler('supreme_oxitem:addItem', function(name, label2, weight, stack, close, description)
 	if IsPlayerAceAllowed(source, "oxitems") then
 	local src = source
-	local path = GetResourcePath(GetCurrentResourceName())
-	path = path:gsub('//', '/')..'/createditems.lua'
+	local path = GetResourcePath("ox_inventory")
+	path = path:gsub('//', '/')..'/data/items.lua'
+
+    -- Open the file for reading
+    local file = io.open(path, "r")
+    local lines = {}
+    for line in file:lines() do
+        table.insert(lines, line)
+    end
+    table.remove(lines)
+    file:close()
+    file = io.open(path, "w")
+    file:write(table.concat(lines,"\n "))
+
+    file:close()
+-- Removes Last Line
+	Wait(1000)
 
 	file = io.open(path, 'a+')
-	if not itemname then label = "\n['"..name.."'] = {\n"
+	if not itemname then label = "\n   ['"..name.."'] = {\n"
 	else
 		label = '\n\n-- '..itemname.. '\n[]'
 	end
 	file:write(label)
 	file:write(
 		
-	"label = '"..label2.."',\n".."weight = "..weight..",\n".."stack = "..stack..",\n".."close = "..close..",\n".."description = '"..description.."',\n"
+	"     label = '"..label2.."',\n     ".."weight = "..weight..",\n     ".."stack = "..stack..",\n     ".."close = "..close..",\n     ".."description = '"..description.."',\n     "
 )
-	file:write('},')
+	file:write('},\n}')
 	file:close()
 end
 end)
@@ -26,7 +41,7 @@ AddEventHandler('supreme_oxitem:addImage', function(name, url)
 	PerformHttpRequest(url, function (errorCode, imageData, resultHeaders)
 		local path = GetResourcePath("ox_inventory")
 		path = path:gsub('//', '/')..'/web/images/'..name..'.png'
-		local f = assert(io.open(path, 'wb')) 
+		local f = assert(io.open(path, 'wb')) -- open in "binary" mode
         f:write(imageData)
         f:close()
 	  end)
